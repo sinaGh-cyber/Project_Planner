2f4dd6d139753c24c36d51465ab2670a15fe2edc
sinaGh-cyber <sinaina1377@gmail.com>
2021-06-19 03:37:50 +0430

    select lists

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,21 @@ class ToolTip{}
 
 class ProjectItem{}
 
-class ProjectList{}
+class ProjectList{
+    constructor(type) {
+        const projItems = document.querySelectorAll(`#${type}-projects li`);
+        // console.log(projIttems);
+        // for (const project of projItems){
+        //     const Item = new ProjectItem(type, project);
+        // }
+    }
+}
 
-class App{}
+class App{
+    static init(){
+        const activeProjectListEl = new ProjectList(`active`);
+        const finishedProjectListEl = new ProjectList(`finished`);
+    }
+}
+
+App.init();

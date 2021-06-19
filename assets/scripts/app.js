013d0f92209b3271ab36da21d0a0c9dfbd790de1
sinaGh-cyber <sinaina1377@gmail.com>
2021-06-19 17:47:36 +0430

    instructor aproch

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,55 +1,42 @@
-class ToolTip {}
+class ToolTip{}
 
-class ProjectItem {
-  goToFinishedListHandler = (chosenBtn) => {
-    this.project.remove();
-    document
-      .getElementById(`finished-projects`)
-      .querySelector(`ul`)
-      .append(this.project);
-      chosenBtn.textContent = `Activate`;
-      chosenBtn.removeEventListener("click", this.goToFinishedListHandler);
-      chosenBtn.addEventListener(`click`, this.goToActivListHandler.bind(null, chosenBtn));
-  };
-  goToActivListHandler = (chosenBtn) => {
-    this.project.remove();
-    document
-      .getElementById(`active-projects`)
-      .querySelector(`ul`)
-      .append(this.project);
-    chosenBtn.textContent = `Finish`;
-    chosenBtn.removeEventListener("click", this.goToActivListHandler);;
-    chosenBtn.addEventListener(`click`, this.goToFinishedListHandler.bind(null, chosenBtn));
-  };
-  constructor(type, project) {
-    this.project = project;
-    if (type === `active`) {
-      this.finishBtn = project.querySelectorAll(`button`)[1];
-      this.finishBtn.addEventListener(`click`, this.goToFinishedListHandler.bind(null, this.finishBtn));
-    } else if (type === `finished`) {
-      this.activateBtn = project.querySelectorAll(`button`)[1];
-      this.activateBtn.addEventListener(`click`, this.goToActivListHandler.bind(null, this.activateBtn));
+class ProjectItem{
+    constructor(ID){
+        this.id = ID;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton()
     }
-    this.moreInfoBtn = project.querySelectorAll(`button`)[0];
-    const ToolTipObj = new ToolTip(this.moreInfoBtn);
-  }
+    connectMoreInfoButton(){}
+
+    connectSwitchButton(){
+        const projectItemEl = document.getElementById(this.id);
+        switchButton = projectItemEl.querySelector(`button:last-of-type`);
+        switchButton.addEventListener(`click`,)
+    }
 }
 
-class ProjectList {
-  constructor(type) {
-    const projItems = document.querySelectorAll(`#${type}-projects li`);
-    // console.log(projIttems);
-    for (const project of projItems) {
-      const Item = new ProjectItem(type, project);
+class ProjectList{
+    projects = [];
+    constructor(type) {
+        const projItems = document.querySelectorAll(`#${type}-projects li`);
+        // console.log(projIttems);
+        for (const project of projItems){
+            this.projects.push(new ProjectItem(project.id));
+        }
     }
-  }
+
+    addProject(){}
+    switchProject(projectId){
+        this.addProject();
+        this.projects.filter(p => p.id !== projectId);
+    }
 }
 
-class App {
-  static init() {
-    const activeProjectListEl = new ProjectList(`active`);
-    const finishedProjectListEl = new ProjectList(`finished`);
-  }
+class App{
+    static init(){
+        const activeProjectListEl = new ProjectList(`active`);
+        const finishedProjectListEl = new ProjectList(`finished`);
+    }
 }
 
 App.init();

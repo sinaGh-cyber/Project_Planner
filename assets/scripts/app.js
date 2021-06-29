66d5a62e9df32f92e305ee607f75ccc90a0cf1b4
sinaGh-cyber <sinaina1377@gmail.com>
2021-06-30 00:41:36 +0430

    tooltip positioning modified

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -31,15 +31,15 @@ class Component {
 
   attach() {
     this.hostElement.insertAdjacentElement(
-      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.insertBefore ? "afterbegin" : "beforeend",
       this.element
     );
   }
 }
 
 class Tooltip extends Component {
-  constructor(closeNotifierFunction, text) {
-    super();
+  constructor(closeNotifierFunction, text, hostElementId) {
+    super(hostElementId);
     this.closeNotifier = closeNotifierFunction;
     this.text = text;
     this.create();
@@ -51,10 +51,23 @@ class Tooltip extends Component {
   };
 
   create() {
-    const tooltipElement = document.createElement('div');
-    tooltipElement.className = 'card';
+    const tooltipElement = document.createElement("div");
+    tooltipElement.className = "card";
     tooltipElement.textContent = this.text;
-    tooltipElement.addEventListener('click', this.closeTooltip);
+    const hostElPosLeft = this.hostElement.offsetLeft;
+    const hostElPosTop = this.hostElement.offsetTop;
+    const hostElPosHight = this.hostElement.clientHeight;
+    const parentElementScrolling = this.hostElement.parentElement.scrollTop;
+
+    const x = this.hostElPosLeft + 20;
+    const y = hostElPosTop + hostElPosHight - parentElementScrolling - 10;
+
+    tooltipElement.style.position = `sticky`;
+    tooltipElement.style.left = x + `px`;
+    tooltipElement.style.top = y + `px`;
+    // tooltipElement.style.position = `sticky`;
+
+    tooltipElement.addEventListener("click", this.closeTooltip);
     this.element = tooltipElement;
   }
 }
@@ -75,9 +88,13 @@ class ProjectItem {
     }
     const projectElement = document.getElementById(this.id);
     const tooltipText = projectElement.dataset.extraInfo;
-    const tooltip = new Tooltip(() => {
-      this.hasActiveTooltip = false;
-    }, tooltipText);
+    const tooltip = new Tooltip(
+      () => {
+        this.hasActiveTooltip = false;
+      },
+      tooltipText,
+      this.id
+    );
     tooltip.attach();
     this.hasActiveTooltip = true;
   }
@@ -85,18 +102,18 @@ class ProjectItem {
   connectMoreInfoButton() {
     const projectItemElement = document.getElementById(this.id);
     const moreInfoBtn = projectItemElement.querySelector(
-      'button:first-of-type'
+      "button:first-of-type"
     );
-    moreInfoBtn.addEventListener('click', this.showMoreInfoHandler.bind(this));
+    moreInfoBtn.addEventListener("click", this.showMoreInfoHandler.bind(this));
   }
 
   connectSwitchButton(type) {
     const projectItemElement = document.getElementById(this.id);
-    let switchBtn = projectItemElement.querySelector('button:last-of-type');
+    let switchBtn = projectItemElement.querySelector("button:last-of-type");
     switchBtn = DOMHelper.clearEventListeners(switchBtn);
-    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+    switchBtn.textContent = type === "active" ? "Finish" : "Activate";
     switchBtn.addEventListener(
-      'click',
+      "click",
       this.updateProjectListsHandler.bind(null, this.id)
     );
   }
@@ -134,15 +151,15 @@ class ProjectList {
   switchProject(projectId) {
     // const projectIndex = this.projects.findIndex(p => p.id === projectId);
     // this.projects.splice(projectIndex, 1);
-    this.switchHandler(this.projects.find(p => p.id === projectId));
-    this.projects = this.projects.filter(p => p.id !== projectId);
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
 class App {
   static init() {
-    const activeProjectsList = new ProjectList('active');
-    const finishedProjectsList = new ProjectList('finished');
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
     activeProjectsList.setSwitchHandlerFunction(
       finishedProjectsList.addProject.bind(finishedProjectsList)
     );
